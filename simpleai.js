693a0330d37dd25b0c325f79c5e3a5bbd4749c2a
Blinktrash <76610232+Blinktrash@users.noreply.github.com>
2023-04-30 16:05:22 -0500

    The AI-like system
    A small test to experiment with weights and bias in javascript. I plan to implement this on another project to analyze stock patterns.

---
+++ simpleai.js
@@ -0,0 +1,28 @@
+// Guessmult can't go over 4 because that would eliminate randomness
+var guessmult = 0; 
+var guessweight = Math.random(1,10);
+var guess;
+if(guessweight + guessmult >= 5){
+  guess = 1;
+} else {
+  guess = 2;
+}
+var userguess = prompt("Select 1 or 2")
+if(userguess == 1){ 
+ if(guess == 1){
+    console.log("Guess Correct!");
+  } else {
+    console.log("Guess Incorrect");
+    guessmult = guessmult+0.5;
+  }
+  console.log("Adjusted Weight: " + guessmult);
+};
+if(userguess == 2){
+  if(guess == 1){
+    console.log("Guess Incorrect!");
+    guessmult = guessmult-0.5;
+  } else {
+    console.log("Guess Correct!");
+  }
+  console.log("Adjusted Weight: " + guessmult);
+};
